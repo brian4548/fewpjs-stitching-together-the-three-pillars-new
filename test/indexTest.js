512ba3b65753ee59a85eb7118c0ea102a209ab75
brian4548 <brianr4548@gmail.com>
2021-01-09 17:59:32 -0600

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-describe("index.js", () => {
+expldescribe("index.js", () => {
   it("exists", () => {
     expect(testVar).to.exist
     expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
